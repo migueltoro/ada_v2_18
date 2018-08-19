575b42b9195f8871bf29991c2914fae28a69bb3b
migueltoro <miguel.toro.bonilla@gmail.com>
2018-08-19 09:44:05 +0200

    Junio/2018

---
+++ java/LpSolve/src/module-info.java
@@ -0,0 +1,14 @@
+
+/**
+ * 
+ * Algorimo para resolver problemas de programación lineal entera
+ * 
+ * @author Miguel Toro
+ *
+ */
+module us.lsi.lpsolve {
+	
+	exports us.lsi.lpsolve.solution;
+
+	
+}
