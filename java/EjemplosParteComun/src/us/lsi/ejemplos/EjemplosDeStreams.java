c64d40d736045ec2f6e563e8dafded584b44b83c
migueltoro <miguel.toro.bonilla@gmail.com>
2018-08-28 10:53:39 +0200

    28/8/2018

--- java/EjemplosParteComun/src/us/lsi/ejemplos/EjemplosDeStreams.java
+++ java/EjemplosParteComun/src/us/lsi/ejemplos/EjemplosDeStreams.java
@@ -1,5 +1,7 @@
 package us.lsi.ejemplos;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import us.lsi.common.SeqAccumulators;
@@ -7,13 +9,15 @@ import us.lsi.common.Streams2;
 import us.lsi.common.Tuple;
 import us.lsi.math.Math2;
 
+
+
 public class EjemplosDeStreams {
 	
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		var s1 = Stream.of(1,2,3,4,5,6,7);
-		var s2 = Stream.of(11,12,13,14,15,16);
-		var r2 = Stream.of(11,12,13,14,15,16);
+		var s1 = List.of(1,2,3,4,5,6,7);
+		var s2 = List.of(11,12,13,14,15,16);
+		var r2 = List.of(11,12,13,14,15,16,34,57);
 		Stream.generate(()->Math2.getEnteroAleatorio(0, 100))
 			.limit(4)
 			.forEach(System.out::println);
@@ -21,13 +25,13 @@ public class EjemplosDeStreams {
 		var rr = Stream.generate(()->Math2.getEnteroAleatorio(0, 100)).filter(x->x%7==0);
 		System.out.println(Streams2.accumulateLeft(rr, SeqAccumulators.first()).get());
 		System.out.println("______");
-		var s3 = r2.map(x->x.toString());
+		var s3 = r2.stream().map(x->x.toString());
 		var a = SeqAccumulators.joiningAccumulator(" ","{","}");
 		var r = Streams2.accumulateLeft(s3, a);
 		System.out.println(r);	
 		System.out.println("______");
 		var s4 = Stream.iterate(0, x->x+1);
-		var s5 = Streams2.zip(s2,s4,(x,y)->Tuple.create(x, y));
+		var s5 = Streams2.zip(s2.stream(),s4,(x,y)->Tuple.create(x, y));
 		s5.forEach(System.out::println);
 		System.out.println("______");
 		var n = 14L;
@@ -44,6 +48,15 @@ public class EjemplosDeStreams {
 						.reduce(1L,(x,y)->x*y);
 					   
 		System.out.println(s7+","+r1+","+Math.pow(b,n));
+		System.out.println("______");
+		var ss = Streams2.elementsAndPosition(r2.stream());
+		var r3 = ss.map(t->t.toString()).collect(Collectors.joining(","));
+		System.out.println(r3);
+		System.out.println("______");
+		var enteros = Stream.iterate(0,x->x+1);
+		var ss2 = Streams2.limit(enteros, 4);
+		var r4 = ss2.map(t->t.toString()).collect(Collectors.joining(","));
+		System.out.println(r4);
 
 	}
 
