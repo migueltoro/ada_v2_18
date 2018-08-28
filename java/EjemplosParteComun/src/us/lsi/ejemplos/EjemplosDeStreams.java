9c30a3e190befdaa4516e55daa951add451345d1
migueltoro <miguel.toro.bonilla@gmail.com>
2018-08-28 20:30:36 +0200

    28/8/2018

--- java/EjemplosParteComun/src/us/lsi/ejemplos/EjemplosDeStreams.java
+++ java/EjemplosParteComun/src/us/lsi/ejemplos/EjemplosDeStreams.java
@@ -1,18 +1,39 @@
 package us.lsi.ejemplos;
 
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import us.lsi.common.Collectors2;
+import us.lsi.common.Multiset;
 import us.lsi.common.SeqAccumulators;
 import us.lsi.common.Streams2;
+import us.lsi.common.Strings2;
 import us.lsi.common.Tuple;
+import us.lsi.common.Tuple2;
 import us.lsi.math.Math2;
-
 
 
 public class EjemplosDeStreams {
 	
+	public static List<Tuple2<Long,Long>> primosPar(Long m, Long n, Integer k){
+		var r = Stream.iterate(Math2.siguientePrimo(m),x->x < n,x->Math2.siguientePrimo(x));
+		var r2 = Streams2.consecutivePairs(r);
+		var rr2 = r2.filter(t->t.v2-t.v1==k).collect(Collectors.toList());
+		return rr2;
+	}
+	
+	public static Stream<Long> divisores(Long n){
+		return Stream.iterate(2L, x-> x <= (long) Math.sqrt(n), x -> x+1).filter(x->n%x==0);
+	}
+	
+	public static Stream<Tuple2<Long,Long>> primos(Long a){
+		var r = Stream.iterate(Math2.siguientePrimo(a),x->Math2.siguientePrimo(x));
+		var r2 = Streams2.consecutivePairs(r);
+		
+		return r2;
+	}
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		var s1 = List.of(1,2,3,4,5,6,7);
@@ -57,7 +78,18 @@ public class EjemplosDeStreams {
 		var ss2 = Streams2.limit(enteros, 4);
 		var r4 = ss2.map(t->t.toString()).collect(Collectors.joining(","));
 		System.out.println(r4);
-
+		System.out.println("______");
+		var pp = primosPar(1000L,100000L,2);
+		Strings2.toConsole(pp,"Pares de primos");
+		System.out.println("______");
+		Long m1 = 1000L;
+		Long n1 = 2000L;
+		Multiset<Long> rr4 = 
+				Stream.iterate(m1,x->x<n1,x->x+1)
+				.flatMap(x->divisores(x))
+				.collect(Collectors2.toMultiset());
+		System.out.println(rr4);
+		System.out.println("______");
 	}
 
 }
