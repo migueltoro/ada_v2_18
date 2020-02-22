708b0aee948f1cfff4cf398a42bbd9dd3fb24f2c
migueltoro <migueltoro@LAPTOP-93NQHUSI.lsi.us.es>
2020-02-22 19:32:56 +0100

    22/02/2020

--- java/EjemplosDeGrafos/src/us/lsi/graphs/examples/RecorridoAnchuraTest.java
+++ java/EjemplosDeGrafos/src/us/lsi/graphs/examples/RecorridoAnchuraTest.java
@@ -1,6 +1,7 @@
 package us.lsi.graphs.examples;
 
 import java.io.PrintWriter;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -13,6 +14,7 @@ import us.lsi.grafos.datos.Carretera;
 import us.lsi.grafos.datos.Ciudad;
 import us.lsi.graphcolors.GraphColors;
 import us.lsi.graphs.Graphs2;
+import us.lsi.graphs.BreadthSearch;
 import us.lsi.graphs.GraphsReader;
 import us.lsi.graphs.manual.RecorridoEnAnchuraManual;
 
@@ -25,6 +27,8 @@ public class RecorridoAnchuraTest {
 						Carretera::ofFormat,
 						Graphs2::simpleWeightedGraph,
 						Carretera::getKm);
+		
+		graph.addVertex(Ciudad.ofName("Londres"));
 		
 		RecorridoEnAnchuraManual<Ciudad,Carretera> ra = RecorridoEnAnchuraManual.of(graph,Ciudad.ofName("Sevilla"));
 		Set<Ciudad> ciudades = ra.graph.vertexSet();
@@ -49,6 +53,9 @@ public class RecorridoAnchuraTest {
 		PrintWriter f2 = Files2.getWriter("ficheros/andaluciaAnchura.gv");
 		de2.exportGraph(graph, f2);
 
+		BreadthSearch<Ciudad,Carretera> ba = BreadthSearch.of(graph,List.of(Ciudad.ofName("Sevilla"),Ciudad.ofName("Londres")));		
+		ba.stream().forEach(c->System.out.println(c));	
+		System.out.println(ba.getDepth(Ciudad.ofName("Londres")));
 	}
 
 
