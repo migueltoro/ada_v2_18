fef781f778940f6bcb9a964d16f63635cf92bc32
migueltoro <migueltoro@LAPTOP-93NQHUSI.lsi.us.es>
2020-07-15 12:48:35 +0200

    15/07/2020

--- java/ParteComun/src/us/lsi/flujossecuenciales/SeqCollector.java
+++ java/ParteComun/src/us/lsi/flujossecuenciales/SeqCollector.java
@@ -7,30 +7,26 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
-public interface SeqCollector<E, B, R> {
+public class SeqCollector<E, B, R> {
 	
-	Supplier<B> supplier();
-	BiFunction<B,E,B> accumulator();
-	Function<B,R> finisher();
-	Predicate<B> isDone();
 	
 	public static <E,B,R> SeqCollector<E,B,R> of(
 			Supplier<B> supplier, 
 			BiFunction<B, E, B> accumulator, 
 			Function<B,R> finisher,
 			Predicate<B> isDone){
-		return new SeqCollectorC<>(supplier,accumulator,finisher,isDone);
+		return new SeqCollector<>(supplier,accumulator,finisher,isDone);
 	}
 	
 	public static <E,B> SeqCollector<E,B,B> of(
 			Supplier<B> supplier, 
 			BiFunction<B, E, B> accumulator){
-		return new SeqCollectorC<>(supplier,accumulator,x->x,x->false);
+		return new SeqCollector<>(supplier,accumulator,x->x,x->false);
 	}
 	
 	public static <E> SeqCollector<E,E,E> of(
 			BinaryOperator<E> accumulator){
-		return new SeqCollectorC<>(()->null,accumulator,x->x,x->false);
+		return new SeqCollector<>(()->null,accumulator,x->x,x->false);
 	}
 	
 	public static <E,B,R> SeqCollector<E,B,R> ofBaseMutable(
@@ -41,39 +37,38 @@ public interface SeqCollector<E, B, R> {
 		return SeqCollector.of(supplier,(b,e)->{accumulator.accept(b, e);return b;},finisher,isDone);
 	}
 	
-	static class SeqCollectorC<E,B,R> implements SeqCollector<E,B,R> {
-		private Supplier<B> supplier;;
-		private BiFunction<B, E, B> accumulator;;
-		private Function<B,R> finisher;
-		private Predicate<B> isDone;
-		public SeqCollectorC(Supplier<B> supplier, 
-				BiFunction<B, E, B> accumulator, 
-				Function<B, R> finisher,
-				Predicate<B> isDone) {
-			super();
-			this.supplier = supplier;
-			this.accumulator = accumulator;
-			this.finisher = finisher;
-			this.isDone = isDone;
-		}
-		@Override
-		public Supplier<B> supplier() {
-			return supplier;
-		}
-		@Override
-		public BiFunction<B, E, B> accumulator() {
-			return accumulator;
-		}
-		@Override
-		public Function<B, R> finisher() {
-			return finisher;
-		}
-		@Override
-		public Predicate<B> isDone() {
-			return isDone;
-		}
-		
+	private Supplier<B> supplier;;
+	private BiFunction<B, E, B> accumulator;;
+	private Function<B,R> finisher;
+	private Predicate<B> isDone;
+	
+	public SeqCollector(Supplier<B> supplier, 
+			BiFunction<B, E, B> accumulator, 
+			Function<B, R> finisher,
+			Predicate<B> isDone) {
+		super();
+		this.supplier = supplier;
+		this.accumulator = accumulator;
+		this.finisher = finisher;
+		this.isDone = isDone;
 	}
-
+	
+	public Supplier<B> supplier() {
+		return supplier;
+	}
+	
+	public BiFunction<B, E, B> accumulator() {
+		return accumulator;
+	}
+	
+	public Function<B, R> finisher() {
+		return finisher;
+	}
+	
+	public Predicate<B> isDone() {
+		return isDone;
+	}
+		
 }
+
 
