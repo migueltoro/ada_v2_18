d636de03016d941306e08ff029adc03c117ee9b2
migueltoro <migueltoro@LAPTOP-93NQHUSI.lsi.us.es>
2021-02-05 19:23:09 +0100

    05/02/2021

--- java/EjemplosAlgoritmos/src/us/lsi/alg/monedas/MonedasHeuristica.java
+++ java/EjemplosAlgoritmos/src/us/lsi/alg/monedas/MonedasHeuristica.java
@@ -10,19 +10,7 @@ import us.lsi.graphs.alg.GreedySearch;
 import us.lsi.graphs.virtual.EGraph;
 
 public class MonedasHeuristica {
-	
-	public static Double heuristica_max(MonedaVertex v1, Predicate<MonedaVertex> goal, MonedaVertex end) {
-		
-		EGraph<MonedaVertex, MonedaEdge> graph = Graphs2.simpleVirtualGraph(v1);
-		
-		GreedySearch<MonedaVertex, MonedaEdge> rr = GraphAlg.greedy(
-				graph,
-				MonedaVertex::accionHeuristica,
-				goal);
-		
-		GraphPath<MonedaVertex, MonedaEdge> path = rr.search();
-		return path.getWeight();
-	}
+
 	
 	public static Double heuristica(MonedaVertex v1, Predicate<MonedaVertex> goal, MonedaVertex end) {
 		
